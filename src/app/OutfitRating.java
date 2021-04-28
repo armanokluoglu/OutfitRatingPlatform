27b0e1015d9ad7a39aef9b21483fe1965d3b5cba
ekremogr365 <68231424+ekremogr365@users.noreply.github.com>
2021-04-28 14:44:26 +0300

    2. window

--- src/app/OutfitRating.java
+++ src/app/OutfitRating.java
@@ -8,14 +8,17 @@ import view.MainFrame;
 
 public class OutfitRating {
 	FrameManager fm;
-	
+	FrameManager fm2;
+
 	public OutfitRating() {
 		this.fm = new FrameManager();
+		this.fm2 = new FrameManager();
 	}
 	
 	public void start() throws UserAlreadyException {
 		Model model = new Model();
         MainFrame view = new MainFrame(model, this.fm);
         MainController controller = new MainController(model, view, this.fm);
+		MainController controller2 = new MainController(model, view, this.fm2);
 	}
 }
